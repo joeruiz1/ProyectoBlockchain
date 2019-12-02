b43cdfb1d56fc39d421f832f29be4de12f6d25a0
joeruiz1 <fernandostiven14yo@gmail.com>
2019-12-02 13:43:52 -0500

    3 files modified

--- src/app/crear-camisas/crear-camisas.component.ts
+++ src/app/crear-camisas/crear-camisas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-crear-camisas',
   templateUrl: './crear-camisas.component.html',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearCamisasComponent implements OnInit {
 
-  constructor() { }
+ public id: string;
+  constructor(private route: ActivatedRoute) {
+  route.params.subscribe(
+    params=>{this.id=params['id']})
+  }
 
   ngOnInit() {
   }
